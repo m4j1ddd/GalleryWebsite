828dbc3953ac6d68eba2e589afafb900d3427918
socoot <majidabdollahi@ut.ac.ir>
2018-02-08 03:11:09 +0330

    Add project files.

---
+++ GalleryWebsite/Default.aspx
@@ -0,0 +1,169 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Website._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head id="Head1" runat="server">
+    <title>گالری عکس اژدها</title>
+    <style type="text/css">
+        .style1
+        {
+            width: 100%;
+        }
+        .style3
+        {
+            width: 137px;
+        }
+        .style4
+        {
+            width: 351px;
+        }
+        .style5
+        {
+            width: 29px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table align="center" class="style1" dir="rtl">
+            <tr>
+                <td colspan="3">
+                <center>
+                    <asp:Image ID="Image1" runat="server" ImageUrl="logo.png" />
+                </center>
+                </td>
+            </tr>
+            <tr>
+                <td class="style4">
+                <table>
+                <tr>
+                <td>
+                    <asp:Button ID="Button3" runat="server" Text="جدیدترین عکس ها" 
+                        onclick="Button3_Click" />
+                </td>
+                <td>
+                    <asp:Button ID="Button4" runat="server" Text="سرگرمی" onclick="Button4_Click" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button5" runat="server" onclick="Button5_Click" Text="علمی" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button6" runat="server" onclick="Button6_Click" Text="ورزشی" />
+                    </td>
+                </tr>
+                </table>
+                </td>
+                <td class="style3">
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+                <td class="style5">
+                    <asp:Button ID="Button2" runat="server" onclick="Button2_Click" Text="جستجو" />
+                </td>
+            </tr>
+            <tr>
+                <td class="style4">
+                    &nbsp;</td>
+                <td class="style3">
+                    &nbsp;</td>
+                <td class="style5">
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="style4">
+                    <asp:Label ID="Label1" runat="server">جدیدترین عکس ها</asp:Label>
+                </td>
+                <td class="style3">
+                    آدرس عکس:</td>
+                <td class="style5">
+                    <asp:FileUpload ID="FileUpload1" runat="server" />
+                </td>
+            </tr>
+            <tr>
+                <td class="style4" rowspan="4">
+                <table>
+                <tr>
+                    <asp:ListView ID="ListView1" runat="server" DataKeyNames="ID" 
+                        style="margin-bottom: 0px">
+                        <ItemTemplate>
+                            <td runat="server">
+                                <asp:Label ID="Label2" runat="server" Text="نام: "></asp:Label><asp:Label ID="namLabel" runat="server" Text='<%# Eval("nam") %>' />
+                                <br />
+                                <asp:Image ID="Image2" runat="server" Height="100px" 
+                                    ImageUrl='<%# Bind("url") %>' Width="100px" />
+                                <br />
+                                <asp:Label ID="Label3" runat="server" Text="موضوع: "></asp:Label>
+                                <asp:Label ID="subLabel" runat="server" Text='<%# Eval("sub") %>' />
+                                <br />
+                            </td>
+                        </ItemTemplate>
+                    </asp:ListView>
+                </tr>
+                </table>
+                </td>
+                <td class="style3">
+                    نام عکس:</td>
+                <td class="style5">
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    موضوع عکس:</td>
+                <td class="style5">
+                    <asp:DropDownList ID="DropDownList1" runat="server">
+                        <asp:ListItem Selected="True">موضوع عکس را انتخاب کنید</asp:ListItem>
+                        <asp:ListItem>سرگرمی</asp:ListItem>
+                        <asp:ListItem>علمی</asp:ListItem>
+                        <asp:ListItem>ورزشی</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    &nbsp;</td>
+                <td class="style5">
+                    <asp:Button ID="Button1" runat="server" Text="آپلود" onclick="Button1_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    &nbsp;</td>
+                <td class="style5">
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="style4" rowspan="4">
+                    &nbsp;</td>
+                <td class="style3">
+                    &nbsp;</td>
+                <td align="center" class="style5">
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    &nbsp;</td>
+                <td class="style5">
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    &nbsp;</td>
+                <td class="style5">
+                
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="style3">
+                    &nbsp;</td>
+                <td class="style5">
+                    &nbsp;</td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ GalleryWebsite/Default.aspx.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.OleDb;
+
+namespace Website
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                String str;
+                str = "provider=Microsoft.ACE.oledb.12.0; Data source=" + Server.MapPath("db/db.accdb") + "; persist security info = false;";
+                OleDbConnection cn = new OleDbConnection(str);
+                cn.Open();
+                String st;
+                st = "select * from table1 order by ID Desc";
+                OleDbCommand cmd = new OleDbCommand(st, cn);
+                OleDbDataReader dr;
+                dr = cmd.ExecuteReader();
+                ListView1.DataSource = dr;
+                ListView1.DataBind();
+                cn.Close();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text != "" && DropDownList1.SelectedIndex != 0)
+            {
+                String type = FileUpload1.PostedFile.ContentType;
+                if (type.Contains("image"))
+                {
+                    String str;
+                    str = "provider=Microsoft.ACE.oledb.12.0; Data source=" + Server.MapPath("db/db.accdb") + "; persist security info = false;";
+                    OleDbConnection cn = new OleDbConnection(str);
+                    cn.Open();
+                    String st;
+                    st = "select ID from table1 order by ID Desc";
+                    OleDbCommand cmd = new OleDbCommand(st, cn);
+                    OleDbDataReader dr;
+                    dr = cmd.ExecuteReader();
+                    int count = 0;
+                    if (dr.Read())
+                    {
+                        count = int.Parse(dr.GetValue(0).ToString());
+                    }
+                    dr.Close();
+                    count = count + 1;
+                    String[] t = new String[2];
+
+                    String nam = FileUpload1.FileName;
+                    t = nam.Split('.');
+                    FileUpload1.SaveAs("D:/Works/GalleryWebsite/GalleryWebsite/images/" + count + "." + t[1]);
+                    String sub = DropDownList1.SelectedValue;
+                    String url = "images/" + count + "." + t[1];
+                    String stcmd;
+                    stcmd = "insert into table1(nam, sub, url) values('" + TextBox1.Text + "', '" + sub + "', '" + url + "')";
+                    cmd.CommandText = stcmd;
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            String str;
+            str = "provider=Microsoft.ACE.oledb.12.0; Data source=" + Server.MapPath("db/db.accdb") + "; persist security info = false;";
+            OleDbConnection cn = new OleDbConnection(str);
+            cn.Open();
+            String st;
+            st = "select * from table1  where nam like '%" + TextBox2.Text + "%' order by ID Desc";
+            OleDbCommand cmd = new OleDbCommand(st, cn);
+            OleDbDataReader dr;
+            dr = cmd.ExecuteReader();
+            ListView1.DataSource = dr;
+            ListView1.DataBind();
+            cn.Close();
+            Label1.Text = "نتایج برای جستجوی '" + TextBox2.Text + "'";
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            String str;
+            str = "provider=Microsoft.ACE.oledb.12.0; Data source=" + Server.MapPath("db/db.accdb") + "; persist security info = false;";
+            OleDbConnection cn = new OleDbConnection(str);
+            cn.Open();
+            String st;
+            st = "select top 5 * from table1 order by ID Desc";
+            OleDbCommand cmd = new OleDbCommand(st, cn);
+            OleDbDataReader dr;
+            dr = cmd.ExecuteReader();
+            ListView1.DataSource = dr;
+            ListView1.DataBind();
+            cn.Close();
+            Label1.Text = "جدیدترین عکس ها";
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            String str;
+            str = "provider=Microsoft.ACE.oledb.12.0; Data source=" + Server.MapPath("db/db.accdb") + "; persist security info = false;";
+            OleDbConnection cn = new OleDbConnection(str);
+            cn.Open();
+            String st;
+            st = "select * from table1 where sub = 'سرگرمی' order by ID Desc";
+            OleDbCommand cmd = new OleDbCommand(st, cn);
+            OleDbDataReader dr;
+            dr = cmd.ExecuteReader();
+            ListView1.DataSource = dr;
+            ListView1.DataBind();
+            cn.Close();
+            Label1.Text = "سرگرمی";
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            String str;
+            str = "provider=Microsoft.ACE.oledb.12.0; Data source=" + Server.MapPath("db/db.accdb") + "; persist security info = false;";
+            OleDbConnection cn = new OleDbConnection(str);
+            cn.Open();
+            String st;
+            st = "select * from table1 where sub = 'علمی' order by ID Desc";
+            OleDbCommand cmd = new OleDbCommand(st, cn);
+            OleDbDataReader dr;
+            dr = cmd.ExecuteReader();
+            ListView1.DataSource = dr;
+            ListView1.DataBind();
+            cn.Close();
+            Label1.Text = "علمی";
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            String str;
+            str = "provider=Microsoft.ACE.oledb.12.0; Data source=" + Server.MapPath("db/db.accdb") + "; persist security info = false;";
+            OleDbConnection cn = new OleDbConnection(str);
+            cn.Open();
+            String st;
+            st = "select * from table1 where sub = 'ورزشی' order by ID Desc";
+            OleDbCommand cmd = new OleDbCommand(st, cn);
+            OleDbDataReader dr;
+            dr = cmd.ExecuteReader();
+            ListView1.DataSource = dr;
+            ListView1.DataBind();
+            cn.Close();
+            Label1.Text = "ورزشی";
+        }
+    }
+}
